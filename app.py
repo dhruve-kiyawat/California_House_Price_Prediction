e4fdaed7bd8ea9ed339d575270b35126923fb996
Dhruve Kiyawat <dhruvekiyawat739@gmail.com>
2024-02-12 10:45:33 +0530

    Added Predicting Functionality

--- app.py
+++ app.py
@@ -23,5 +23,14 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    input = scaler.transform(np.array(data).reshape(1, -1))
+    print(input)
+    output = model.predict(input)[0]
+    output = round(output, 2) * 100000
+    return render_template("home.html", prediction_text = f"The House Price Prediction is {output} $")
+
 if __name__=="__main__":
     app.run(debug = True)
